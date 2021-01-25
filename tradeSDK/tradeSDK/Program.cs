d0e32072de3ef97ab3b4ef781466c9c3a91c0c3e
BlankaKorvo <BlankaKorvo@gmail.com>
2021-01-25 17:26:02 +0300

    configing trade strategy

--- tradeSDK/tradeSDK/Program.cs
+++ tradeSDK/tradeSDK/Program.cs
@@ -22,12 +22,7 @@ namespace tradeSDK
                 .MinimumLevel.Debug()
                 .WriteTo.Console()
                 .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-
-            Log.Information("Hello, world!"); 
-            
-
-
+                .CreateLogger();        
 
             Market market = new Market();
             SandboxContext context = new Auth().GetSanboxContext();
@@ -42,16 +37,16 @@ namespace tradeSDK
 
             //DPO config 
             int dpoPeriod = 20;
-            int dpoAverageAngleCountLong = 2;
-            double dpoAverageAngleConditionLong = 20;
+            int dpoAverageAngleCountLong = 3;
+            double dpoAverageAngleConditionLong = 30;
             int dpoAverageAngleCountFromLong = 4;
-            double dpoAverageAngleConditionFromLong = -5;
+            double dpoAverageAngleConditionFromLong = -20;
 
             decimal longLastDpoCondition = 0;
             decimal fromLongDpoCondition = 0;
 
             //Ema config
-            int emaPeriod = 10;
+            //int emaPeriod = 10;
             decimal ichimokuTenkansenPriceDeltaCount = 0.12M;
 
             //Super Trend config
@@ -60,7 +55,7 @@ namespace tradeSDK
 
             //Ichimoku
             int ichimokuDeltaAngleCountLong = 2;
-            double ichimokuTenkanSenAngleLong = 10;
+            double ichimokuTenkanSenAngleLong = 20;
 
             while (true)
             {
@@ -68,17 +63,13 @@ namespace tradeSDK
                 stopWatch.Start();
                 try
                 {
-
-                    Log.Information("Hello, world!");
                     int count = 0;
-
+                    Log.Information("Start cicle");
                     CandleList candleList = await market.GetCandlesTinkoff(context, figi, candleInterval, CandleCount);
-                    Console.WriteLine(stopWatch.ElapsedMilliseconds);
                     Orderbook orderbook = await context.MarketOrderbookAsync(figi, 1);
-                    Console.WriteLine(stopWatch.ElapsedMilliseconds);
                     if (orderbook.Asks.Count == 0)
                     {
-                        Console.WriteLine(DateTime.Now + "  биржа не фурычит");
+                        Log.Information("Биржа по инструменту " + figi + " не работает");
                         Thread.Sleep(sleep);
                         continue;
                     }
@@ -92,17 +83,19 @@ namespace tradeSDK
                     var deltaPrice = (ask + bid) / 2;
 
                     List<SmaResult> smaForDpo = Serialization.SmaData(candleList, dpoPeriod, deltaPrice);
-                    List<EmaResult> ema = Serialization.EmaData(candleList, emaPeriod, deltaPrice);
+                    //List<EmaResult> ema = Serialization.EmaData(candleList, emaPeriod, deltaPrice);
                     List<DpoResult> dpo = Serialization.DpoData(candleList, dpoPeriod, deltaPrice);
                     List<SuperTrendResult> superTrand = Serialization.SuperTrendData(candleList, superTrandPeriod, superTrandSensitive, deltaPrice);
                     List<IchimokuResult> ichimoku = Serialization.IchimokuData(candleList, deltaPrice);
 
                     bool ichimokuLongLine(List<IchimokuResult> ichimoku, decimal? price)
                     {
-                        if (ichimoku.Last().TenkanSen > ichimoku.Last().KijunSen
+                        if (ichimoku.Last().TenkanSen >= ichimoku.Last().KijunSen
                             && price > ichimoku.Last().SenkouSpanA
                             && price > ichimoku.Last().SenkouSpanB
-                            && price > ichimoku.Last().TenkanSen)
+                            && price > ichimoku.Last().TenkanSen
+                            && ichimoku.Last().TenkanSen > ichimoku.Last().SenkouSpanA
+                            && ichimoku.Last().TenkanSen > ichimoku.Last().SenkouSpanB)
                         {
                             return true;
                         }
@@ -137,7 +130,7 @@ namespace tradeSDK
                     }
 
                     decimal? lastDpo = dpo.Last().Dpo;
-                   
+
 
                     double DeltaDegreeAngle(List<decimal?> values)
                     {
@@ -153,9 +146,8 @@ namespace tradeSDK
                         return summ / (countDelta - 1);
                     }
 
-                    decimal? emaPriceDelta = 100 - (ema.Last().Ema * 100 / deltaPrice); //Насколько далеко убежала цена от Ema
+                    // decimal? emaPriceDelta = 100 - (ema.Last().Ema * 100 / deltaPrice); //Насколько далеко убежала цена от Ema
 
-                    Console.WriteLine(stopWatch.ElapsedMilliseconds);
                     var portfolio = await context.PortfolioAsync();
 
 
@@ -183,113 +175,82 @@ namespace tradeSDK
                         using (StreamWriter sw = new StreamWriter("operation", true, System.Text.Encoding.Default))
                         {
                             sw.WriteLine(DateTime.Now + @" Buy  price: " + ask + " UpperBand: " + superTrand.Last().UpperBand + " LowerBand: " + superTrand.Last().LowerBand + " DPO: " + lastDpo + " SmaPriceDelta: " + ichimokuTenkansenPriceDelta + " DpoDegreeAverageAngle: " + DpoDegreeAverageAngle(dpo, dpoAverageAngleCountLong));
-                            sw.WriteLine(@" Ichimoku: " + ichimoku.Last().TenkanSen + ichimoku.Last().KijunSen + ichimoku.Last().SenkouSpanA + ichimoku.Last().SenkouSpanB);
+                            sw.WriteLine(@" Ichimoku: " + ichimoku.Last().TenkanSen + " " + ichimoku.Last().KijunSen + " " + ichimoku.Last().SenkouSpanA + " " + ichimoku.Last().SenkouSpanB);
 
                             sw.WriteLine();
                         }
                     }
                     else if (count > 0
-                            && (superTrand.Last().LowerBand == null
-                                || lastDpo < fromLongDpoCondition
-                                || DpoDegreeAverageAngle(dpo, dpoAverageAngleCountFromLong) < dpoAverageAngleConditionFromLong))
+                            && (
+                            (lastDpo < fromLongDpoCondition && superTrand.Last().LowerBand == null)
+                                || (DpoDegreeAverageAngle(dpo, dpoAverageAngleCountFromLong) < dpoAverageAngleConditionFromLong) && superTrand.Last().LowerBand == null))
                     {
                         await context.PlaceLimitOrderAsync(new LimitOrder(figi, 1, OperationType.Sell, bid));
                         using (StreamWriter sw = new StreamWriter("operation", true, System.Text.Encoding.Default))
                         {
                             sw.WriteLine(DateTime.Now + @" Sell  price: " + bid + " UpperBand: " + superTrand.Last().UpperBand + " LowerBand: " + superTrand.Last().LowerBand + " DPO: " + lastDpo + " SmaPriceDelta: " + ichimokuTenkansenPriceDelta + " DpoDegreeAverageAngle: " + DpoDegreeAverageAngle(dpo, dpoAverageAngleCountLong));
-                            sw.WriteLine(@" Ichimoku: " + ichimoku.Last().TenkanSen + ichimoku.Last().KijunSen + ichimoku.Last().SenkouSpanA + ichimoku.Last().SenkouSpanB);
+                            sw.WriteLine(@" Ichimoku: " + ichimoku.Last().TenkanSen + " " + ichimoku.Last().KijunSen + " " + ichimoku.Last().SenkouSpanA + " " + ichimoku.Last().SenkouSpanB);
 
                             sw.WriteLine();
                         }
                     }
+                    Log.Information("Price: " + deltaPrice);
 
-                    //Console.WriteLine(stopWatch.ElapsedMilliseconds);
-
-                    Console.WriteLine("********");
-                    Console.WriteLine("Long condition:");
-                    Console.WriteLine();
-                    Console.WriteLine("Price: " + deltaPrice);
-                    Console.WriteLine();
                     if (superTrand.Last().UpperBand != null)
                     {
-                        Console.WriteLine("Go to Sell");
+                        Log.Information("SuperTrand: Sell");
                     }
                     else
                     {
-                        Console.WriteLine("Go to Buy");
+                        Log.Information("SuperTrand: Buy");
                     }
-                    Console.WriteLine();
-                    Console.WriteLine("lastDpo: " + lastDpo);
-                    Console.WriteLine();
-                    Console.WriteLine("IchimokuTenkansenPriceDelta: " + ichimokuTenkansenPriceDelta);
-                    Console.WriteLine();
-                    Console.WriteLine("DpoDegreeAverageAngle: " + DpoDegreeAverageAngle(dpo, dpoAverageAngleCountLong));
-                    Console.WriteLine("ichimoku: ");
-                    Console.WriteLine("     TenkanSen: " + ichimoku.Last().TenkanSen);
-                    Console.WriteLine("     KijunSen: " + ichimoku.Last().KijunSen);
-                    Console.WriteLine("     SenkouSpanA: " + ichimoku.Last().SenkouSpanA);
-                    Console.WriteLine("     SenkouSpanB: " + ichimoku.Last().SenkouSpanB);
-                    Console.WriteLine("     SenkouSpanB: " + ichimoku.Last().SenkouSpanB);
-                    Console.WriteLine("     ichmokuTenkansenAngleLong: " + ichmokuTenkansenDegreeAverageAngle(ichimoku, ichimokuDeltaAngleCountLong));
+                    Log.Information("lastDpo: " + lastDpo);
+                    Log.Information("IchimokuTenkansenPriceDelta: " + ichimokuTenkansenPriceDelta);
 
-                    //Console.WriteLine();
-                    //Console.WriteLine("lastDpo: " + lastDpo);
-                    //Console.WriteLine("deltaAngleFourDpo: " + deltaAngleFourDpo);
-                    //Console.WriteLine("SmaPriceDelta: " + EmaPriceDelta);
-                    //Console.WriteLine();
-                    
 
-                    //Console.WriteLine("lastDpo: " + lastDpo + dpo.Last().Dpo);
-                    //Console.WriteLine("twoLastDpo: " + twoLastDpo + " "+ dpo[dpo.Count - 2].Dpo);
-                    //Console.WriteLine("threeLastDpo: " + threeLastDpo + " " + dpo[dpo.Count - 3].Dpo);
-                    //Console.WriteLine("fourLastDpo: " + fourLastDpo + " " + dpo[dpo.Count - 4].Dpo);
-
-                    //Console.WriteLine();
-                    //Console.WriteLine("Ichimoku: " + ichimoku.Last().TenkanSen + " " + ichimoku.Last().KijunSen + " " + ichimoku.Last().SenkouSpanA + " " + ichimoku.Last().SenkouSpanB);
-
-                    //Console.WriteLine("Obv: " + obv.Last().Obv);
-                    //Console.WriteLine("ObvSma: " + obv.Last().ObvSma);
-                    //Console.WriteLine("deltaPriceOneLast: " + deltaPriceOneLast);
-                    //Console.WriteLine("deltaPraiceTwoLast: " + deltaPraiceTwoLast);
-                    //Console.WriteLine();
-
-                    //Console.WriteLine("deltaPrice: " + deltaPrice);
-
-                    //Console.WriteLine();
-                    //Console.WriteLine("closePrice: " + closePrice);
-                    //Console.WriteLine("lastPrise: " + lastPrice);
-                    //Console.WriteLine("bid: " + bid);
-                    //Console.WriteLine("ask: " + ask);
-                    //Console.WriteLine();
-
-                    //if (superTrand.Last().UpperBand != null)
-                    //{
-                    //    Console.WriteLine("Go to Sell. UpperBand: " + superTrand.Last().UpperBand);
-                    //}
-                    //else
-                    //{
-                    //    Console.WriteLine("Go to Buy. LowerBand: " + superTrand.Last().LowerBand);
-                    //}
-
-                    //Console.WriteLine("SuperTrend: " + superTrand.Last().SuperTrend);
-                    //Console.WriteLine();
-                    //Console.WriteLine("*****");
-                    //Console.WriteLine();
+                    Log.Information("DpoDegreeAverageAngle: " + DpoDegreeAverageAngle(dpo, dpoAverageAngleCountLong));
+                    Log.Information("ichimoku: ");
+                    Log.Information("     TenkanSen: " + ichimoku.Last().TenkanSen);
+                    Log.Information("     KijunSen: " + ichimoku.Last().KijunSen);
+                    Log.Information("     SenkouSpanA: " + ichimoku.Last().SenkouSpanA);
+                    Log.Information("     SenkouSpanB: " + ichimoku.Last().SenkouSpanB);
+                    Log.Information("     SenkouSpanB: " + ichimoku.Last().SenkouSpanB);
+                    Log.Information("     ichmokuTenkansenAngleLong: " + ichmokuTenkansenDegreeAverageAngle(ichimoku, ichimokuDeltaAngleCountLong));
                 }
+
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex);
+                    Log.Information(ex.ToString());
                     sleep += 10;
                 }
                 finally
                 {
                 }
-                DynamicSleep(sleep);
-                Console.WriteLine("Sleep!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: " + sleep);
-                Thread.Sleep(sleep);
+
+                sleep = DSleep(sleep);
+
                 stopWatch.Stop();
-                Console.WriteLine(stopWatch.ElapsedMilliseconds);
+                Log.Information(stopWatch.ElapsedMilliseconds.ToString());
+                Log.Information("Stop cicle");
+
+                Log.Information("Sleep: " + sleep);
+                Thread.Sleep(sleep);
             }
+        }
+
+        private static int DSleep(int sleep)
+        {
+            sleep -= 1;
+            if (sleep < 0)
+            {
+                sleep = 0;
+            }
+            else if (sleep > 1000)
+            {
+                sleep = 1000;
+            }
+
+            return sleep;
         }
 
         static int DynamicSleep(int sleep)
